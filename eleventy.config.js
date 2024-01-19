21d09f14181a8ab968570b654e0e9ce04b35f4e8
Rupert Angermeier <rupert.angermeier@gmail.com>
2024-01-19 09:07:47 +0100

    basic styling
    - use includes for default page layout
    - add font Cabin
    - add colors
    - move CSS and JS to /static

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("style.css");
-    eleventyConfig.addPassthroughCopy("js/map.js");
+    eleventyConfig.addPassthroughCopy({"static": "."});
    
     eleventyConfig.addPassthroughCopy("node_modules/ol/dist/ol.js");
     eleventyConfig.addPassthroughCopy("node_modules/ol/ol.css");
