4516f657215428f077439b1ab4eb1ed40380659d
Rupert Angermeier <rupert.angermeier@gmail.com>
2024-02-23 12:38:23 +0100

    upgrade to 11ty v3.0.0-alpha5

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({"static": "."});
    
     eleventyConfig.addPassthroughCopy("node_modules/ol/dist/ol.js");
